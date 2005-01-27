c867d0f188c08acfcadf2ac60984f19915748b2a
rudolf <rudolf>
2005-01-27 18:17:48 +0000

    added globals for GPS receiver

--- src/elekIOServ/elekIOServ.h
+++ src/elekIOServ/elekIOServ.h
@@ -16,3 +16,18 @@ struct SyncFlagType {
 	BOOL MaskChange;
 };  /*SyncFlagType */
 
+// ========================
+// Globals for GPS
+// ========================
+
+unsigned char port[256] = "/dev/ttyS1";   // serial device used for the incoming GPS data
+unsigned char pDataBuffer[1024];
+
+char aProgramName[] = "GPS";              // needed for serial.c 's debug output
+char *progname = (char *) aProgramName;   // export pointer to debug string
+
+int verbose = 255;                        // be very verbose
+long baud = 9600;                         // serial baudrate
+volatile char ucDataReadyFlag = 0;        // Data ready flag
+int fdGPS = -1;                           // file descriptor for serial communication
+char ucPortOpened = 0;                    // flag for the timer routine wether port is available or not
