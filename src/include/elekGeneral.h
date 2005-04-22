87b3606b6295580578c8f63cc54c445b9ea32a9d
rudolf <rudolf>
2005-04-22 10:52:52 +0000

    added extra path for files in RAMDISK (e.g. for status.bin)

--- src/include/elekGeneral.h
+++ src/include/elekGeneral.h
@@ -1,9 +1,18 @@
-/* general definitions */
+/*
+* $RCSfile: elekGeneral.h,v $ last changed on $Date: 2005-04-22 10:52:52 $ by $Author: rudolf $
+*
+* $Log: elekGeneral.h,v $
+* Revision 1.2  2005-04-22 10:52:52  rudolf
+* added extra path for files in RAMDISK (e.g. for status.bin)
+*
+*
+*/
 
 #define MAX_FILENAME_LEN 128
 #define GENERIC_BUF_LEN 256
 
 #define DATAPATH   "./data"
+#define RAMDISKPATH "/lift/ramdisk"
 
 #ifndef bool
 typedef unsigned char bool;
