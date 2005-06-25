0cee1f0b434c4de55acb406221d9796f91274de1
rudolf <rudolf>
2005-06-25 18:19:48 +0000

    new function for keyboard handling added

---
+++ src/commTools/getch.c
@@ -0,0 +1,39 @@
+/* portable_getch.c */
+#include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
+
+static struct termios new_io;
+static struct termios old_io;
+
+/* Funktion schaltet das Terminal in den cbreak-Modus:        */
+/* Kontrollflag ECHO und ICANON auf 0 setzen                  */
+/* Steuerzeichen: Leseoperation liefert 1 Byte VMIN=1 VTIME=1 */
+int cbreak(int fd) {
+   /*Sichern unseres Terminals*/
+   if((tcgetattr(fd, &old_io)) == -1)
+      return -1;
+   new_io = old_io;
+   /*Wir verändern jetzt die Flags für   den cbreak-Modus*/
+   new_io.c_lflag = new_io.c_lflag & ~(ECHO|ICANON);
+   new_io.c_cc[VMIN] = 0;
+   new_io.c_cc[VTIME]= 0;
+
+   /*Jetzt setzen wir den cbreak-Modus*/
+   if((tcsetattr(fd, TCSAFLUSH, &new_io)) == -1)
+      return -1;
+   return 1;
+}
+
+int restoreinput(void){
+   /*Alten Terminal-Modus wiederherstellen*/
+   tcsetattr(STDIN_FILENO, TCSANOW, &old_io);
+};
+
+int getch(void) {
+   int c;
+
+   c = getchar();
+   return c;
+}
