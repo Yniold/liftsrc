700e302f4d4f7596ef244719a6e04e558f09fd0e
martinez <martinez>
2007-02-27 17:39:46 +0000

    DyelaserTemp warning on if 1.5 degrees off

--- src/matlab/Dyelaser.m
+++ src/matlab/Dyelaser.m
@@ -218,7 +218,7 @@ else
     set(handles.txtPRef,'BackgroundColor',[0.7,0.7,0.7]);
 end
 
-if TDyelaser(lastrow)>horusdata.TDyelaserset+2 | TDyelaser(lastrow)<horusdata.TDyelaserset-2
+if TDyelaser(lastrow)>horusdata.TDyelaserset+1.5 | TDyelaser(lastrow)<horusdata.TDyelaserset-1.5
     set(handles.txtTDyelaser,'BackgroundColor','r');
 else
     set(handles.txtTDyelaser,'BackgroundColor',[0.7,0.7,0.7]);
