740a93710a3c065da0d204089d4df18e1f5b6aed
Markus Rudolf <armdev@armdev-vm.(none)>
2011-02-28 16:52:49 +0100

    added files for I2C communication thread

---
+++ src/elekIOGSB/gsb.h
@@ -0,0 +1,53 @@
+#ifndef GSB_H
+#define GSB_H
+
+#define REJECTION_50HZ_60HZ 	(0)
+#define REJECTION_50HZ 			(1)
+#define REJECTION_60HZ  		(2)
+
+extern int DAC_SetChannel(int iFD, int iChannel, unsigned short usDACValue);
+extern int ADC_GetChannel(int iFD, int iADC, int iChannel, int *piReading);
+extern double Ohm2Temperature(double dResistance);
+
+#pragma pack(push)
+#pragma pack(1)
+
+struct sLTCConfigField
+{
+	// LSB come first
+	unsigned char LTDontUse0:3;
+	unsigned char LTSpeedBit:1;
+	unsigned char LTMainsRejection:2;
+	unsigned char LTInternalSensorBit:1;
+	unsigned char LTNewConfigBit:1;
+	
+
+	// MSB follows
+	unsigned char LTChannel:3;
+	unsigned char LTOdd:1;
+	unsigned char LTSingleEndedBit:1;
+	unsigned char LTEnableBit:1;
+	unsigned char LTPreambleBits:2;
+};
+
+union sLTCConfig
+{
+	struct sLTCConfigField LCField;
+	unsigned char LCArray[2];
+};
+
+struct sLTCDataField
+{
+	unsigned char LTDontUse0:6;
+	signed int LTReading:25;
+	unsigned char LTSignedBit:1; 		
+};
+
+union sLTCData
+{
+	struct sLTCDataField LDField;
+	unsigned char LCArray[4];
+};
+ 
+#pragma pack(pop)
+#endif
