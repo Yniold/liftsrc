72dffd90ca33dc524587476031397ecea17296ae
user at Lift <lift@lift>
2011-07-14 12:39:28 +0000

    changed MFC propene and C3F6 max flow settings in FlyDetection.m

--- matlab/FlyDetection.m
+++ matlab/FlyDetection.m
@@ -2052,7 +2052,7 @@ function editMFCProp_Callback(hObject, eventdata, handles)
 
 % Hints: get(hObject,'String') returns contents of editMFCProp as text
 %        str2double(get(hObject,'String')) returns contents of editMFCProp as a double
-MaxFlow=200;
+MaxFlow=200*.41;
 setMFC=str2double(get(hObject,'String'));
 if setMFC<0 | isnan(setMFC) setMFC=0; end
 if setMFC>MaxFlow setMFC=MaxFlow; end
@@ -2084,7 +2084,7 @@ function editMFCC3F6_Callback(hObject, eventdata, handles)
 
 % Hints: get(hObject,'String') returns contents of editMFCC3F6 as text
 %        str2double(get(hObject,'String')) returns contents of editMFCC3F6 as a double
-MaxFlow=500;
+MaxFlow=2000*0.249;
 setMFC=str2double(get(hObject,'String'));
 if setMFC<0 | isnan(setMFC) setMFC=0; end
 if setMFC>MaxFlow setMFC=MaxFlow; end
